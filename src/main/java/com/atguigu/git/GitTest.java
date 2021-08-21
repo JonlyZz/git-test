732314ef86dfb288b0b6602815c711705502eef9
zz <506841746@qq.com>
2021-08-21 17:09:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Zz
+ * @create 2021-08-21 17:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
